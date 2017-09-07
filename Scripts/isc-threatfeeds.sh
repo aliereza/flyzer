b5c15c64704bda3e8053c19df5fc379200c6bfb8
Control Panel <control@Controls-MacBook-Pro.local>
2017-09-07 23:51:47 +0430

    Feed script

---
+++ Scripts/isc-threatfeeds.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#This script fetch the list of dangerous IP address from isc.sans.edu and update the "Botnet.csv" file
+#The mentioned file is being used by translate filter that check if the DST IP address is being listed.
+
+$api_url = "https://isc.sans.edu/api/threatlist/"
+$curl = which curl
+
+echo `$curl -XGET $api_url "zeuscc"`
